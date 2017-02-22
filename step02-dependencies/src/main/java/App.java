c3d80a8e3b8e9c0fd034c1ac1c37a63d342cb82f
Lukasz Szwajkowski <lukasz.szwajkowski@allegrogroup.com>
2017-02-22 21:50:13 +0100

    playing with gradle

--- step02-dependencies/src/main/java/App.java
+++ step02-dependencies/src/main/java/App.java
@@ -7,6 +7,6 @@ public class App {
     }
 
     public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+        System.out.println(new NameProvider().provide());
     }
 }
